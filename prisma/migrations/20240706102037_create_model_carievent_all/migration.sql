b2482d992a4783699f02285ac5b94cf3911c8b3c
Ungguh Faizaturrohman <otemoos@outlook.com>
2024-07-06 17:28:57 +0700

    migrate model to database

---
+++ prisma/migrations/20240706102037_create_model_carievent_all/migration.sql
@@ -0,0 +1,221 @@
+-- CreateTable
+CREATE TABLE `admin` (
+    `id` INTEGER NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `admin_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `blog` (
+    `id` INTEGER NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `headline` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `adminId` INTEGER NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `creator` (
+    `id` INTEGER NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `creatorName` VARCHAR(191) NULL,
+    `logo` VARCHAR(191) NULL,
+    `banner` VARCHAR(191) NULL,
+    `description` VARCHAR(191) NULL,
+    `contact` VARCHAR(191) NULL,
+    `eventId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `media` (
+    `id` INTEGER NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `creatorId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `address` (
+    `id` INTEGER NOT NULL,
+    `street` VARCHAR(191) NOT NULL,
+    `kecamatan` VARCHAR(191) NOT NULL,
+    `kabupaten` VARCHAR(191) NOT NULL,
+    `postalCode` VARCHAR(191) NOT NULL,
+    `creatorId` INTEGER NULL,
+    `customerId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `guarantor` (
+    `id` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `contact` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `idNumber` INTEGER NOT NULL,
+    `creatorId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `legal` (
+    `id` INTEGER NOT NULL,
+    `npwpId` INTEGER NOT NULL,
+    `npwpImage` VARCHAR(191) NOT NULL,
+    `legalLicense` INTEGER NOT NULL,
+    `legalLicenseImage` VARCHAR(191) NOT NULL,
+    `creatorId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `customer` (
+    `id` INTEGER NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `fullName` VARCHAR(191) NOT NULL,
+    `idNumber` INTEGER NOT NULL,
+    `idImage` VARCHAR(191) NOT NULL,
+    `contact` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `customer_idNumber_key`(`idNumber`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Tickets` (
+    `id` INTEGER NOT NULL,
+    `ticketId` VARCHAR(191) NOT NULL,
+    `customerId` INTEGER NULL,
+    `transactionId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `event` (
+    `id` INTEGER NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `banner` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `detail` VARCHAR(191) NULL,
+    `eventStart` DATETIME(3) NOT NULL,
+    `eventEnd` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ticket-class` (
+    `id` INTEGER NOT NULL,
+    `class` VARCHAR(191) NOT NULL,
+    `itemTotal` INTEGER NOT NULL,
+    `price` INTEGER NOT NULL,
+    `eventId` INTEGER NULL,
+    `orderId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `category` (
+    `id` INTEGER NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `describtion` VARCHAR(191) NOT NULL,
+    `eventId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `location` (
+    `id` INTEGER NOT NULL,
+    `street` VARCHAR(191) NOT NULL,
+    `kecamatatn` VARCHAR(191) NOT NULL,
+    `kabupaten` VARCHAR(191) NOT NULL,
+    `postalCode` VARCHAR(191) NOT NULL,
+    `eventId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `order` (
+    `id` INTEGER NOT NULL,
+    `totalPrice` INTEGER NOT NULL,
+    `customerId` INTEGER NULL,
+    `transactionId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `transaction` (
+    `id` INTEGER NOT NULL,
+    `customerId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `blog` ADD CONSTRAINT `blog_adminId_fkey` FOREIGN KEY (`adminId`) REFERENCES `admin`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `creator` ADD CONSTRAINT `creator_eventId_fkey` FOREIGN KEY (`eventId`) REFERENCES `event`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `media` ADD CONSTRAINT `media_creatorId_fkey` FOREIGN KEY (`creatorId`) REFERENCES `creator`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `address` ADD CONSTRAINT `address_creatorId_fkey` FOREIGN KEY (`creatorId`) REFERENCES `creator`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `address` ADD CONSTRAINT `address_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `customer`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `guarantor` ADD CONSTRAINT `guarantor_creatorId_fkey` FOREIGN KEY (`creatorId`) REFERENCES `creator`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `legal` ADD CONSTRAINT `legal_creatorId_fkey` FOREIGN KEY (`creatorId`) REFERENCES `creator`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Tickets` ADD CONSTRAINT `Tickets_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `customer`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Tickets` ADD CONSTRAINT `Tickets_transactionId_fkey` FOREIGN KEY (`transactionId`) REFERENCES `transaction`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ticket-class` ADD CONSTRAINT `ticket-class_eventId_fkey` FOREIGN KEY (`eventId`) REFERENCES `event`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ticket-class` ADD CONSTRAINT `ticket-class_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `order`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `category` ADD CONSTRAINT `category_eventId_fkey` FOREIGN KEY (`eventId`) REFERENCES `event`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `location` ADD CONSTRAINT `location_eventId_fkey` FOREIGN KEY (`eventId`) REFERENCES `event`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `order` ADD CONSTRAINT `order_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `customer`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `order` ADD CONSTRAINT `order_transactionId_fkey` FOREIGN KEY (`transactionId`) REFERENCES `transaction`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `transaction` ADD CONSTRAINT `transaction_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `customer`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
