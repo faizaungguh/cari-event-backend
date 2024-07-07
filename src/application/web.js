dc68150c9e25613128c6a442ca86f6b14a90bbc7
Ungguh Faizaturrohman <otemoos@outlook.com>
2024-07-07 12:28:52 +0700

    Revert "add security request"
    This reverts commit 1e8cc7a9e208b47dd46f9f4b0c4cb703126505a1.

--- src/application/web.js
+++ src/application/web.js
@@ -1,30 +1,9 @@
 import express from 'express';
-import cors from 'cors';
-import xss from 'xss';
 import { privateRouter } from '../route/private.js';
 import { errorMiddleware } from '../middleware/error.js';
-import helmet from 'helmet';
 
 export const web = express();
 
-//json and security
-web.use(express.urlencoded());
 web.use(express.json());
-web.use(cors());
-web.use((req, res, next) => {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Origin', '*'); //ell url can acccess
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATH, OPTIONS'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-web.use(helmet());
-web.use(xss());
-
-//Route
 web.use('/api/v1', privateRouter);
-
 web.use(errorMiddleware);
