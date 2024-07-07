01c873e6e99bc852432385ee0607a2e67012d16d
Ungguh Faizaturrohman <otemoos@outlook.com>
2024-07-07 15:59:20 +0700

    add public router

--- src/application/web.js
+++ src/application/web.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { privateRouter } from '../route/private.js';
+import { publicRouter } from '../route/public.js';
 import { errorMiddleware } from '../middleware/error.js';
 
 export const web = express();
 
 web.use(express.json());
 web.use('/api/v1/private', privateRouter);
-web.use('/api/v1/public')
+web.use('/api/v1/public', publicRouter);
 web.use(errorMiddleware);
