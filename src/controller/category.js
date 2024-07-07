cfe38037ca858c3a1050bb05c4479d7bf21e6905
Ungguh Faizaturrohman <otemoos@outlook.com>
2024-07-08 05:03:35 +0700

    add create category

--- src/controller/category.js
+++ src/controller/category.js
@@ -1,4 +1,16 @@
-const create = async (req, res, next) => {};
+import category from '../service/category.js';
+
+const create = async (req, res, next) => {
+  try {
+    const result = await category.add(req.body);
+    res.status(200).json({
+      data: result,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
 const update = async (req, res, next) => {};
 const list = async (req, res, next) => {};
 const select = async (req, res, next) => {};
--- src/service/category.js
+++ src/service/category.js
@@ -1,4 +1,32 @@
-const add = async () => {};
+import { prismaClient } from '../application/database.js';
+import { createValidation } from '../validations/category.js';
+import { ResponseError } from '../error/response-error.js';
+import { validate } from '../validations/validation.js';
+
+const add = async (req) => {
+  const category = validate(createValidation, req);
+  const countCategory = await prismaClient.category.count({
+    where: {
+      type: category.type,
+    },
+  });
+
+  if (countCategory === 1) {
+    throw new ResponseError(
+      400,
+      'Kategori tersebut sudah ada, pilih nama kategori lain'
+    );
+  }
+
+  return prismaClient.category.create({
+    data: category,
+    select: {
+      type: true,
+      describtion: true,
+    },
+  });
+};
+
 const update = async () => {};
 const selectAll = async () => {};
 const selectId = async () => {};
